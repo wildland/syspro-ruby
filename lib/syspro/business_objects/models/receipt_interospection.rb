f9c3bc21266290d20fd6e884debac6f69751f18e
Chad Zink <chad@wild.land>
2019-01-17 11:19:26 -0800

    Expanded PORTOR
    Added some needed attributes for Labbee project

--- lib/syspro/business_objects/models/receipt_interospection.rb
+++ lib/syspro/business_objects/models/receipt_interospection.rb
@@ -9,7 +9,22 @@ module Syspro
                       :counted_quantity_complete,
                       :delivery_note,
                       :certificate,
-                      :lot
+                      :lot,
+                      :grn_number,
+                      :concession,
+                      :cost_basis,
+                      :notation,
+                      :reference,
+                      :grn_source,
+                      :bins
+        def initialize
+          @bins = []
+        end
+      end
+
+      class InterospectionBin
+        attr_accessor :bin_location,
+                      :bin_quantity
       end
     end
   end
